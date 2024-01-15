d81bab7ecc7745353ae171689c2a4ebfb1069feb
Peter Fitzgerald <peterfitzgerald81@gmail.com>
2024-01-15 16:07:19 +0000

    Completed Game_Object class

---
+++ src/Animation_Application.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+public class Animation_Application extends JFrame implements Runnable{
+
+    //member data
+    private static final Dimension WindowSize = new Dimension(600, 600);
+    private static final int Num_Game_Objects = 30;
+    private Game_Object[] Game_Object_Array = new Game_Object[Num_Game_Objects];
+    // Constructor
+    public Animation_Application(){
+
+    }
+
+    public void run(){
+
+    }
+
+    public void paint(Graphics g){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/Game_Object.java
@@ -0,0 +1,41 @@
+import java.awt.*;
+public class Game_Object {
+
+    // member data
+    private double x,y;
+    private final Color c;
+
+    public Game_Object(){
+        x = Math.random()*600;
+        y = Math.random()*600;
+        c = new Color((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
+    }
+
+    public void move(){
+        int r = (int)(Math.random()*12);
+        if(r < 3) {
+            y += 3;
+        }
+        else if(r < 6){
+            x -= 3;
+        }
+        else if(r < 9){
+            x += 3;
+        }
+        else{
+            y -=3;
+        }
+    }
+
+    public void paint(Graphics g){
+        g.setColor(c);
+        g.fillRect((int)x, (int)y, 40, 40);
+    }
+
+
+
+
+
+
+
+}
